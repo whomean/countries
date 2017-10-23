ecc0982c5860725a1db9f8dd0a6d425efad4b2c0
whomean <l.humin@gmail.com>
2017-10-23 22:42:04 +0200

    added support for error

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,23 @@ function searchCountries() {
     if(!countryName.length) countryName = 'Poland';
   console.log(countryName);
   
-  $.getJSON(url+countryName, getCountry);
+  $.getJSON(url+countryName, getCountry).fail(function() {throwError();});
 }
 
 function getCountry(input) {
-   // console.log(input);
-  countriesList.empty();
-  input.forEach(function(item) {
-    var li = $('<li>', { 'class': 'country' }).appendTo(countriesList);
-    var ul = $('<ul>').appendTo(li);
-    var flag = $('<li>', { 'class': 'flag' }).appendTo(ul);
-    $('<li>', {'class': 'country_name'}).text(item.name).appendTo(ul);
-    $('<li>').text("Capital: " + item.capital).appendTo(ul);
-    $('<img>', {'src': item.flag}).appendTo(flag);
-  });
+    console.log(input);
+  	countriesList.empty();
+  	input.forEach(function(item) {
+	    var li = $('<li>', { 'class': 'country' }).appendTo(countriesList);
+	    var ul = $('<ul>').appendTo(li);
+	    var flag = $('<li>', { 'class': 'flag' }).appendTo(ul);
+	    $('<li>', {'class': 'country_name'}).text(item.name).appendTo(ul);
+	    $('<li>').text("Capital: " + item.capital).appendTo(ul);
+	    $('<img>', {'src': item.flag}).appendTo(flag);
+	});
+}
+
+function throwError() {
+	countriesList.empty();
+	$('<li>', {'class': 'error'}).text("Nie znaleziono").appendTo($('#countries'));
 }
